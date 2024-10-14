a0e323bba7e3d3a55cd4c502cf8853b428751a68
Hozek-V <janvystup@seznam.cz>
2024-10-14 19:05:14 +0200

    books database

---
+++ Test web/book_store/books/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    # author = models.CharField(max_length=255)
+    isbn = models.CharField(max_length=30)
+    description = models.CharField(max_length=500)
+
+    def __str__(self):
+        return f"{self.title} {self.isbn}"
