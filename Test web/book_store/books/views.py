a770d15bbfe130b475f3e62d342b53cc0c3f8211
Hozek-V <janvystup@seznam.cz>
2024-10-14 21:10:15 +0200

    Class view Náhled seznamu

--- Test web/book_store/books/views.py
+++ Test web/book_store/books/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Books
+from .forms import BookForm
 
-# Create your views here.
+
+class BookListView(ListView):
+    model = Books
+    template_name = 'book_list.html'
+
+
+class BookDetailView(DetailView):
+    model = Books
+    template_name = 'book_detail.html'
+
+
+class BookCreateView(CreateView):
+    model = Books
+    form_class = BookForm
+    template_name = 'book_form.html'
+    success_url = reverse_lazy('book_list')
+
+
+class BookUpdateView(UpdateView):
+    model = Books
+    form_class = BookForm
+    template_name = 'book_form.html'
+    success_url = reverse_lazy('book_list')
+
+
+class BookDeleteView(DeleteView):
+    model = Books
+    template_name = 'book_confirm_delete.html'
+    success_url = reverse_lazy('book_list')
