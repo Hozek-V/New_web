38769db816e48cd58a03837cdbbe2bbaa1253295
Hozek-V <janvystup@seznam.cz>
2024-10-21 20:34:55 +0200

    insert_files

--- Test web/book_store/book_api/urls.py
+++ Test web/book_store/book_api/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', json_book_list, name='json_book_list'),
     path('<int:id>/', json_book_detail, name='json_book_detail'),
     path('test/', my_json_view, name='test'),
+    path('file/', my_file, name='file'),
+    path('img/', my_img, name='img'),
 ]
--- Test web/book_store/book_api/views.py
+++ Test web/book_store/book_api/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, FileResponse
 from books.models import Books
 from django.core import serializers
 import json
+from django.conf import settings
+import os
 
 def my_json_view(request):
     data = {
@@ -23,3 +25,15 @@ def json_book_detail(request, id):
     data = Books.objects.get(id=id)
     data = serializers.serialize('json', [data])
     return JsonResponse(data, safe=False)
+
+
+def my_file(request):
+    file_path = os.path.join(settings.MEDIA_ROOT, 'files', 'ukol.txt')
+
+    #return FileRespose(open(file_path, 'rb'), filename='download.txt') #zobrazení
+    return FileResponse(open(file_path, 'rb'), as_attachment=True, filename='download.txt')
+
+def my_img(request):
+    file_path = os.path.join(settings.MEDIA_ROOT, 'img', 'anchor.png')
+
+    return FileResponse(open(file_path, 'rb'), as_attachment=True, filename='download.png')
